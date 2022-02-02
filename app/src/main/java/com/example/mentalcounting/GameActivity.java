12e8d7d1d86ef7d711445f7e299c259dd4667439
Jules <jules.timmerman@etud.u-picardie.fr>
2022-02-02 19:10:36 +0100

    Fin du visuel

--- app/src/main/java/com/example/mentalcounting/GameActivity.java
+++ app/src/main/java/com/example/mentalcounting/GameActivity.java
@@ -1,23 +1,25 @@
 package com.example.mentalcounting;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class GameActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        associateOpenActivityToButton(R.id.buttonScore, ScoreActivity.class);
-        associateOpenActivityToButton(R.id.buttonGame, GameActivity.class);
+        setContentView(R.layout.activity_game);
+        associateOpenActivityToButton(R.id.main,MainActivity.class);
 
         ColorDrawable colorDrawable
                 = new ColorDrawable(Color.parseColor("#AB76F4"));
@@ -25,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
         // Set BackgroundDrawable
         getSupportActionBar().setBackgroundDrawable(colorDrawable);
     }
+
+
 
     private void associateOpenActivityToButton(int id, Class activity){
         Button resultButton = findViewById(id);
@@ -41,9 +45,29 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater menuInflater = getMenuInflater();
-        menuInflater.inflate(R.menu.mainmenu, menu);
+        menuInflater.inflate(R.menu.gamemenu, menu);
 
         return super.onCreateOptionsMenu(menu);
     }
 
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id){
+            case R.id.ResetScore:
+                //Reset scores
+                break;
+            case R.id.Score:
+                Intent intent = new Intent(this, ScoreActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.Previous:
+                finish();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
--- app/src/main/java/com/example/mentalcounting/ScoreActivity.java
+++ app/src/main/java/com/example/mentalcounting/ScoreActivity.java
@@ -1,23 +1,26 @@
 package com.example.mentalcounting;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class ScoreActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        associateOpenActivityToButton(R.id.buttonScore, ScoreActivity.class);
-        associateOpenActivityToButton(R.id.buttonGame, GameActivity.class);
+        setContentView(R.layout.activity_score);
+        Button resultButton = findViewById(R.id.buttonPrevious);
+        resultButton.setOnClickListener(view -> finish());
 
         ColorDrawable colorDrawable
                 = new ColorDrawable(Color.parseColor("#AB76F4"));
@@ -41,9 +44,29 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater menuInflater = getMenuInflater();
-        menuInflater.inflate(R.menu.mainmenu, menu);
+        menuInflater.inflate(R.menu.scoremenu, menu);
 
         return super.onCreateOptionsMenu(menu);
     }
 
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id){
+            case R.id.Game:
+                Intent intent = new Intent(this, GameActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.ResetScore:
+                //Reset scores
+                break;
+            case R.id.Previous:
+                finish();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
